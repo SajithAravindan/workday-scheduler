b23d23e9fd58fc956fa35aecc084265936b21d60
Sajith Aravindan <sajitharavindan11@gmail.com>
2023-08-03 22:08:12 -0400

    Worked on commenting on JS & HTML files

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,81 +4,103 @@
 
 // Global Variables.
 var today = dayjs();
-var strCurrentHour = today.format('h');
-var strCurrentAMPM = today.format('A');
-var dtEventDate = dayjs(today).format('MMDYYYY')
-var intCurrentHour = dayjs().hour();
+
+var strCurrentAMPM = today.format('A');//Returns AM / PM
+var dtEventDate = dayjs(today).format('MMDYYYY')//Date Stamp to use in local Storage
+var intCurrentHour = dayjs().hour();// Current Hour
 var divContainer = $('#container');
 var divSaveStatus = $('#idSaveStatus');
-var strPastCss = "past";
-var strPresentCss = "present";
-var strFutureCss = "future";
+var strPastCss = "past"; //Css for past events time slot
+var strPresentCss = "present";//Css for present events time slot
+var strFutureCss = "future";//Css for future events time slot
 
-const intFromTime = 9;
-const intToTime = 5;
+const intFromTime = 15;//start Time
+const intToTime = 17;//End Time
 
-
-
+//Function on Startup of Application
 function init() {
-  var divDisplayCurrentDay = $('#currentDay');
-  var strDisplayedCurrDate = today.format('dddd MMMM, D');
-  divDisplayCurrentDay.text(strDisplayedCurrDate);  
+  var divDisplayCurrentDay = $('#currentDay');//Place holder to display Current Day
+  var strDisplayedCurrDate = today.format('dddd MMMM, D');// Current Date
+  divDisplayCurrentDay.text(strDisplayedCurrDate);
   //Event Handler for Button Click
   $('#container').on('click', ':button', fnSaveEvent);
   //start calender
   startCal();
 }
 
-function startCal() {  
+//Function to create the Block to create the Event Mgmt Section
+function startCal() {
   divSaveStatus.hide();
-  var objTimeObject = { //Time object that holds time to displar & Required Hours    
+  var objTimeObject = { //Time object that holds time to display & Required Hours    
     HourstoDisplay: ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'],
-    TimeHours: [9, 10, 11, 12, 13, 14, 15, 16, 17] // Correct answers index in the anwers object
+    TimeHours: [9, 10, 11, 12, 13, 14, 15, 16, 17] // Hours - 24 Hour Format    
   }
-
-  //alert(objTimeObject.HourstoDisplay.length)
+  //Loop through the Time Object
   for (var i = 0; i < objTimeObject.HourstoDisplay.length; i++) {
+    //Local Variables
     let strDivClass = '';
     let strDivId = 'hour-';
     let strTxtAreaId = 't-';
     let strTxtAreaContent = '';
-    let strTxtAreaStatus = "enabled";
 
-    if (strCurrentAMPM == "AM" && intCurrentHour < intFromTime)
+    if (strCurrentAMPM == "AM" && intCurrentHour < intFromTime)//Midnight to From Time (9am)
       strDivClass = strFutureCss;
     else
-      strDivClass = fnAssignClass(objTimeObject.TimeHours[i]);
-
-    if (strDivClass == strPastCss) strTxtAreaStatus = "disabled";
+      strDivClass = fnAssignClass(objTimeObject.TimeHours[i]);//Calls the function to assign Class base on time
 
     strDivId = strDivId + objTimeObject.HourstoDisplay[i];
     strTxtAreaId = strTxtAreaId + objTimeObject.HourstoDisplay[i];
 
-    var tempArr = JSON.parse(localStorage.getItem('calDayEvents')); //check if theres a event at this timeslot on this day
+    var arrEventLocalStorage = JSON.parse(localStorage.getItem('calDayEvents')); //check if theres a event at this timeslot on this day
 
-    if (tempArr != null) { //as long as its not empty try eventDate: dtEventDate
-      for (let index = 0; index < tempArr.length; index++) { //loop over all saved calDayEvents
-        if ((objTimeObject.HourstoDisplay[i] == tempArr[index].eventTime) && (dtEventDate == tempArr[index].eventDate)) {
-          strTxtAreaContent = tempArr[index].eventDescription;
-          strTxtAreaId = 'saved-item';
+    if (arrEventLocalStorage != null) { //Not empty
+      for (let index = 0; index < arrEventLocalStorage.length; index++) { //loop over all saved calDayEvents
+        if ((objTimeObject.HourstoDisplay[i] == arrEventLocalStorage[index].eventTime) && (dtEventDate == arrEventLocalStorage[index].eventDate)) {
+          strTxtAreaContent = arrEventLocalStorage[index].eventDescription;
+          strTxtAreaId = 'existing-data';//Tag as existing data
         }
       }
     }
-    //create the Time block Div
-    var elm = '<div ' +
-      'class="row time-block ' + strDivClass + '" ' +
-      'id="' + strDivId + '"> <div class="col-2 col-md-1 hour text-center py-3" id="' + objTimeObject.HourstoDisplay[i] + '">' + objTimeObject.HourstoDisplay[i] + '</div> ' +
-      '<textarea ' + strTxtAreaStatus + '  class="col-8 col-md-10 description" rows="3" id="' + strTxtAreaId + ' ">' + strTxtAreaContent + ' </textarea> ' +
-      '<button class="btn saveBtn col-2 col-md-1" aria-label="save" id="' + objTimeObject.HourstoDisplay[i] + '"><i class="fas fa-save" aria-hidden="true"></i></button>' +
-      '</div>';
-    $(elm).appendTo(divContainer);
+    //Create the container Div for the Event Block
+    var divTimeBlock = document.createElement('div');
+    divTimeBlock.setAttribute('class', 'row time-block ' + strDivClass + '');
+    divTimeBlock.setAttribute('id', '' + strDivId + '');
+    //Create Div displaying the Hour
+    var divHourBlock = document.createElement('div');
+    divHourBlock.setAttribute('class', 'col-2 col-md-1 hour text-center py-3');
+    divHourBlock.textContent = objTimeObject.HourstoDisplay[i];
+    divHourBlock.setAttribute('id', '' + objTimeObject.HourstoDisplay[i] + '');
+    //Create the Input element to add event - Textarea
+    var txtEventDesc = document.createElement('textarea');
+    txtEventDesc.setAttribute('class', 'col-8 col-md-10 description');
+    txtEventDesc.setAttribute('rows', '2');
+    txtEventDesc.setAttribute('id', '' + strTxtAreaId + '');
+    if (strDivClass == strPastCss) txtEventDesc.setAttribute('disabled', 'disabled');
+    txtEventDesc.textContent = strTxtAreaContent;
+    //Create the Save Event button
+    var btnSaveEvent = document.createElement('button');
+    btnSaveEvent.setAttribute('class', 'btn saveBtn col-2 col-md-1');
+    btnSaveEvent.setAttribute('aria-label', 'save');
+    btnSaveEvent.setAttribute('id', '' + objTimeObject.HourstoDisplay[i] + '');
+    //Create the save icon
+    var imgSave = document.createElement('i');
+    imgSave.setAttribute('class', 'fas fa-save');
+    imgSave.setAttribute('aria-hidden', 'true');
+    //Add the icon to the button
+    btnSaveEvent.append(imgSave);
+    //Add all items to the container Div
+    divTimeBlock.append(divHourBlock);
+    divTimeBlock.append(txtEventDesc);
+    divTimeBlock.append(btnSaveEvent);
+    //append rows onto the container
+    divContainer.append(divTimeBlock); 
   }
 };
 
 //Function to assign relevant classes for the Divs
 function fnAssignClass(intDivTimeSlot) {
   let strDivClass = '';
-  if (intCurrentHour == intDivTimeSlot) strDivClass = strPresentCss;
+  if (intCurrentHour == intDivTimeSlot) strDivClass = strPresentCss;//current Hour = Hour of the Slot
   else if (intDivTimeSlot < intCurrentHour) strDivClass = strPastCss;
   else strDivClass = strFutureCss;
   return strDivClass;
@@ -86,41 +108,41 @@ function fnAssignClass(intDivTimeSlot) {
 
 //Function to Add Event to Local Storage
 function fnSaveEvent(event) {
-  var tempArr = []; //initialize array
-  var strTxtContent = this.parentElement.children[1].value;
-  if (localStorage.getItem('calDayEvents') === null) { //if memory is empty make new array
-    //make a new array because we dont have one
+  var arrEventData = []; //initialize array
+  var strTxtContent = this.parentElement.children[1].value; //get Value from Textarea
+  if (localStorage.getItem('calDayEvents') === null) { //if local storage is null
+    //make a new array 
     if (strTxtContent != '') {
-      tempArr = [{ //array of objects
-        eventDate: dtEventDate, //day of the year in 365 format so dont have to look at months or month days
-        eventTime: this.id, //set as 9am or 5pm format
-        eventDescription: strTxtContent, //whatever text is in the textbox
+        arrEventData = [{ //array of objects
+        eventDate: dtEventDate, //current date
+        eventTime: this.id, //Current time as same as the Id of the button
+        eventDescription: strTxtContent, //content from the textbox
       }];
     }
   } else {
-    //trying to add to current memeory
-    tempArr = JSON.parse(localStorage.getItem('calDayEvents')); //fill array with exsisting memory
-    if (this.parentElement.children[1].id == 'saved-item') { //if item is tagged as already having data stored delete previous
-      for (let index = 0; index < tempArr.length; index++) { //go find the data that was already stored in this timeslot
-        if ((tempArr[index].eventDate === dtEventDate) && (tempArr[index].eventTime === this.id)) {
-          tempArrSliceFront = tempArr.slice(0, index);
-          tempArrSliceBack = tempArr.slice(index + 1, tempArr.length);
-          tempArr = []; //set array back to empty
-          tempArr = tempArr.concat(tempArrSliceFront); //everything before the current index
-          tempArr = tempArr.concat(tempArrSliceBack); //everything after the current index
+    //if local storage is not null
+    arrEventData = JSON.parse(localStorage.getItem('calDayEvents')); //fill array with exsisting data
+    if (this.parentElement.children[1].id == 'existing-data') { //if tagged as data stored - delete previous
+      for (let index = 0; index < arrEventData.length; index++) { //find the stored data for this timeslot
+        if ((arrEventData[index].eventDate === dtEventDate) && (arrEventData[index].eventTime === this.id)) {
+          arrEventDataSliceFront = arrEventData.slice(0, index);
+          arrEventDataSliceBack = arrEventData.slice(index + 1, arrEventData.length);
+          arrEventData = []; //set array back to empty
+          arrEventData = arrEventData.concat(arrEventDataSliceFront); //everything before the current index
+          arrEventData = arrEventData.concat(arrEventDataSliceBack); //everything after the current index
         }
       }
     }
-    tempObject = { //temp object to build out the new data to add to the array
-      eventDate: dtEventDate, //day of the year in 365 format so dont have to look at months or month days
-      eventTime: this.id, //set as 9am or 5pm format
-      eventDescription: this.parentElement.children[1].value, //whatever text is in the textbox
+    var arrNewEventData = { //object for the new data to add to the array
+      eventDate: dtEventDate, //current date
+      eventTime: this.id, //Current time as same as the Id of the button
+      eventDescription: strTxtContent, //content from the textbox
     }
-    tempArr.push(tempObject); //push new data into the array to be stored with other stuff
+    arrEventData.push(arrNewEventData); //push new data into the array
   }
-  divSaveStatus.show(); //Status
-  this.parentElement.children[1].id = 'saved-item'; //tag saved descriptions so we remember to erase previous data if overwritten
-  localStorage.setItem('calDayEvents', JSON.stringify(tempArr)) //   
+  divSaveStatus.show(); //Status  
+  this.parentElement.children[1].id = 'existing-data'; //tagged as data stored
+  localStorage.setItem('calDayEvents', JSON.stringify(arrEventData)) //  store locally 
 }
 
 init();
